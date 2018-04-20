6c10ce9986ca74d659e891008c89a5f05f9f5397
Alex322ban <31739245+Alex322ban@users.noreply.github.com>
2018-04-20 17:44:27 -0500

    1

--- Desarrollo/Code vb/Adm_Hotel/CapaConexion/Conexion.vb
+++ Desarrollo/Code vb/Adm_Hotel/CapaConexion/Conexion.vb
@@ -2,6 +2,7 @@
 Public Class Conexion
     Private CN As New SqlConnection("Persist Security Info=False;Integrated Security=true;Initial Catalog=DB_HOTEL;server=(local)\SQLEXPRESS")
 
+
     Public ReadOnly Property getCN As SqlConnection
         Get
             Return CN
